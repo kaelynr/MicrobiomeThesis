83c9640a551df0f464988c797ce547085deb89df
kaelynr <73552131+kaelynr@users.noreply.github.com>
2020-11-25 13:20:22 -0500

    Chi square tested updated

--- ChiSquareTest.R
+++ ChiSquareTest.R
@@ -6,32 +6,38 @@
 setwd("~/University/University 2020-2021/thesis/MicrobiomeThesis")
 library(readxl)
 library(tidyverse)
-SampleResultsMicrobiome <- read_excel("SampleResultsMicrobiome.xlsx")
+SampleResultsMicrobiome <- read_excel("samplemicro.xlsx")
 View(SampleResultsMicrobiome)
 
-# frequencies of different fields of study
-autoResults <- SampleResultsMicrobiome %>%
-  mutate(fieldOfStudy1 = word(.$fieldOfStudy, sep = ", "),
-         fieldOfStudy2 = word(.$fieldOfStudy, start = 2L, sep = ", "))
+#nested core only
+#remove NA values/rows
+sampleresults <- na.omit(SampleResultsMicrobiome)
+view(sampleresults)
 
-autoResultsFreq <- table(c(autoResults$fieldOfStudy1, autoResults$fieldOfStudy2)) %>%
-  as.data.frame() %>% rename("fieldOfStudy" = "Var1")
-
-View(autoResultsFreq)
-
-#what does this do when the cell has 2 or more terms in it?
-chisq.field <- chisq.test(autoResultsFreq$Freq)
-chisq.field
-summary(chisq.field)
-
-# make table with counts of papers from field and core definition used
-tFieldCoreAll <- table(SampleResultsMicrobiome$fieldOfStudy, SampleResultsMicrobiome$typeOfCoreMicrobiome)
+# table for field and nested core
+tFieldCoreNest <- table(sampleresults$fieldOfStudy, sampleresults$typeOfCoreMicrobiome)
 # complete the chi-square test
-cFieldCoreAll <- chisq.test(tFieldCoreAll) 
-cFieldCoreAll
+cFieldCoreNest <- chisq.test(tFieldCoreNest) 
+cFieldCoreNest
+summary(cFieldCoreNest)
+
+# table for field and common core
+tFieldCoreCommon <- table(SampleResultsMicrobiome$fieldOfStudy, SampleResultsMicrobiome$commonCore)
+# complete the chi-square test
+cFieldCoreCommon <- chisq.test(tFieldCoreCommon) 
+cFieldCoreCommon
+summary(cFieldCoreCommon)
+
+# table for field and temporal core
+tFieldCoreTemp <- table(SampleResultsMicrobiome$fieldOfStudy, SampleResultsMicrobiome$temporalCore)
+# complete the chi-square test
+cFieldCoreTemp <- chisq.test(tFieldCoreTemp) 
+cFieldCoreTemp
+summary(cFieldCoreTemp)
 
 # make table with counts of papers from positive/negative interaction and core definition used
 tInteractionCoreAll <- table(SampleResultsMicrobiome$positiveOrNegativeInteraction, SampleResultsMicrobiome$typeOfCoreMicrobiome)
 # complete the chi-square test
 cInteractionCoreAll <- chisq.test(tInteractionCoreAll) 
 cInteractionCoreAll
+

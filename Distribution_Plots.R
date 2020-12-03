0904e18ab180d4eeb5c1efce650bdaa2b795abc7
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2020-12-03 15:45:58 -0500

    Update Distribution_Plots.R
    New comments

--- Distribution_Plots.R
+++ Distribution_Plots.R
@@ -13,21 +13,23 @@ theme_set(theme_minimal())
 micro <- read_excel("micro.xlsx")
 View(micro)
 
-#creating the ggplot
-micro.plot<- ggplot(data=micro, aes(x=fieldOfStudy)) +
+# creating the ggplot for field of study
+fieldPlot<- ggplot(data=micro, aes(x=fieldOfStudy)) +
   geom_bar(fill="steelblue") +
   labs(x = "Field of Study", y = "Number of Articles")
-micro.plot
+fieldPlot
 
 # viridis colours are colour-blind friendly
 
-corePlot <- ggplot(micro, aes(x = nestedCore, 
+
+corePlot <- ggplot(micro, aes(x = nestedCore, # possible to do intersection here instead
                               fill = interaction(commonCore, temporalCore))) +
   geom_bar() +
   # changing the x axis tick labels
+  # discrete b/c the levels are unique (not on a continuous scale)
   scale_x_discrete(labels = c("Ecological", "Functional", "Host-adapted", "None")) +
   # changing the interaction legend
-  # also adds viridis color scheme
+  # also adds viridis color scheme (d for discrete as above)
   scale_fill_viridis_d(labels = c("Neither", "Common Only", "Temporal Only", "Both")) +
   # adjust axis and legend names
   labs(x = "Nested Core Definition", y = "Number of Articles", 
